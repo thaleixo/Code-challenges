097131fabf0aeeeb9153bdf2825ff3dd75dc27de
thaleixo <thiagohenriquealeixo1@gmail.com>
2025-02-17 17:14:57 -0300

    Resolving StaticInitBlock problem

--- HackerRank/Java/Introduction/[7] JavaStaticInitBlock.java
+++ HackerRank/Java/Introduction/[7] JavaStaticInitBlock.java
@@ -27,3 +27,23 @@ Otherwise, print "java.lang.Exception: Breadth and height must be positive" with
 
 */
 
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+       Scanner scan = new Scanner(System.in);
+       
+       int a = scan.nextInt();
+       int b = scan.nextInt();
+       
+       if( a <= 0 || b <= 0 ){
+         System.out.println("java.lang.Exception: Breadth and height must be positive");
+       }else{
+         System.out.println(b * a);
+       }
+    }
+}
